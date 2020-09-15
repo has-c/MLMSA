6c5922b50d7c257db1f86c0848a5b5a344935bed
Hasnain <has-c@github.com>
2020-09-16 02:14:15 +1200

    ground truth tables

---
+++ .ipynb_checkpoints/testing-checkpoint.py
@@ -0,0 +1,14 @@
+import pandas 
+
+#bonding_agent: either Ub, Cis, Trans, Ox
+def ground_truth(bonding_agent):
+
+    tolerance = 10 #30 Da
+
+    if bonding_agent == 'Ub':
+
+
+    pass
+
+def eval_accuracy():
+    pass
