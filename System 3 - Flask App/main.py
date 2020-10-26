d986f7a3d51f7d586056ba20a9deada6b0ff938a
Cheena <hcheena@kpmg.co.nz>
2020-10-26 15:25:58 +1300

    scaffold from basic upload app

--- System 3 - Flask App/main.py
+++ System 3 - Flask App/main.py
@@ -26,6 +26,40 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 def home():
     return render_template("welcome.html")
     
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+
+    if request.method == "POST":
+        # grab filename
+        filename = request.files["file"].filename
+        fcln = request.form["fcln"]
+        cnss = request.form["cnss"]
+
+        if filename == "":
+            # nothing is uploaded
+            flash("empty upload")
+            return render_template("home.html")
+        if fcln == "" or cnss == "":
+            flash("please fill in all text")
+            return render_template("home.html")
+
+        # some file is entered
+        # check it is the valid file type
+        if not (allowed_file(filename)):
+            flash("wrong file type")
+            return render_template("home.html")
+
+        # file is submitted and file type is correct so now grab the file
+        # to do - permission error when trying to save file to uploads folder
+        filename_secure = secure_filename(filename)
+        request.files["file"].save(os.path.join(upload_path, filename_secure))
+
+        # run external python module
+        tax_charter.create_Tax_Charter(filename_secure, fcln, cnss)
+
+        # download
+    return render_template("home.html", filename=filename_secure)
+    
     
 if __name__ == "__main__":
     app.run(debug=True)
