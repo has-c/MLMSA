64b2dbab1b02e6bbe17a5ace37a11c35bdb49ed1
Hasnain <has-c@github.com>
2020-10-26 14:44:09 +1300

    System 3: flask welcome page

--- System 3 - Flask App/main.py
+++ System 3 - Flask App/main.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return 'Tool for Binding Site Prediction'
+    return render_template("welcome.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
