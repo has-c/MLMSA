62fee21704f108e6f8f77d27bf715a28e29472a9
Hasnain <has-c@github.com>
2020-10-26 15:14:20 +1300

    bridging commit

--- System 3 - Flask App/main.py
+++ System 3 - Flask App/main.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
 
-@app.route('/')
-def welcome():
+UPLOAD_FOLDER = 'uploads//'
+ALLOWED_EXTENSIONS = {'xlsx', 'csv'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))   
+
     return render_template("welcome.html")
 
-
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    
 if __name__ == "__main__":
     app.run(debug=True)
