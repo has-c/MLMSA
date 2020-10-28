8746646338d1991bf0c22539e8d295e715b45dfe
Hasnain <has-c@github.com>
2020-10-28 23:06:51 +1300

    System 3: file upload works

--- System 3 - Flask App/app.py
+++ System 3 - Flask App/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, flash, request, redirect, url_for, render_template, send_file
 from werkzeug.utils import secure_filename
 
 ALLOWED_EXTENSIONS = {'xlsx', 'csv'}
@@ -30,30 +30,39 @@ def upload():
 
     if request.method == "POST":
         # grab filename
-        filename = request.files["file"].filename
+        unbound_filename = request.files["unbound_file"].filename
+        bound_filename = request.files["bound_file"].filename
+        compound_file = request.files["compound_file"].filename
 
-        if filename == "":
+        if unbound_filename == "" or bound_filename == "" or compound_file == "":
             # nothing is uploaded
-            flash("empty upload")
+            flash("Please upload all required files")
             return render_template("home.html")
 
         # some file is entered
         # check it is the valid file type
-        if not (allowed_file(filename)):
-            flash("wrong file type")
+        allowed_files_mask = allowed_file(unbound_filename) and allowed_file(bound_filename) and allowed_file(compound_file)
+        if not allowed_files_mask:
+            flash("Wrong File Type. Please only upload xlsx files. ")
             return render_template("home.html")
 
         # file is submitted and file type is correct so now grab the file
         # to do - permission error when trying to save file to uploads folder
-        filename_secure = secure_filename(filename)
-        request.files["file"].save(os.path.join(upload_path, filename_secure))
+        download_uploaded_file(bound_filename, request, "bound_file")
+        download_uploaded_file(unbound_filename, request, "unbound_file")
+        download_uploaded_file(compound_file, request, "unbound_file")
 
         # run external python module
         
 
         # download
 
-    return render_template("home.html", filename=filename_secure)
+
+    return render_template("home.html")
+
+def download_uploaded_file(filename, request, file_id):
+    filename_secure = secure_filename(filename)
+    request.files[file_id].save(os.path.join(upload_path, filename_secure))
     
     
 #download code 
